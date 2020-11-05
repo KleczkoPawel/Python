29e7700eaff29193254d04b6ad1c7c85fbb5e744
KleczkoPawel <72909980+KleczkoPawel@users.noreply.github.com>
2020-11-05 08:45:57 +0100

    Add files via upload

--- lista2/zadanie10.py
+++ lista2/zadanie10.py
@@ -1,15 +1,6 @@
-x = range(3,100,3)
-for n in x:
-    print(n)
-    
-    
-    
-    
-    
- 
-    
-        
-    
-    
-
-
+a = list(range(3 , 100, 3))
+print (a)
+del a[5 : 100 : 3]
+print (a)
+wynik = sum(a)/len(a)
+print (wynik)
